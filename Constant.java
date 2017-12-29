869512894b4d5fa5bdd63e071104a0069db53ca6
SayeedG <sayeed.gulmahamad@gmail.com>
2017-12-28 20:40:10 -0500

    Delete Constant.java

--- Constant.java
+++
@@ -1,9 +0,0 @@
-// Author: Sayeed Gulmahamad
-public class Constant {
-	//login user: ADMIN
-		public static final String ADMIN = "admin";
-		//login user: user
-		public static final String USER = "user";
-		//password for both users
-		public static final String PASSWORD = "CS370";
-}
